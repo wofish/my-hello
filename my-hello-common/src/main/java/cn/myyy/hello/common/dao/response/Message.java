7b413b53ba5ae9bd9713a6562fcd66970a7ca511
syfei49@163.com <syfei49@163.com>
2017-12-12 22:40:04 +0800

    public return

---
+++ my-hello-common/src/main/java/cn/myyy/hello/common/dao/response/Message.java
@@ -0,0 +1,21 @@
+package cn.myyy.hello.common.dao.response;
+
+import java.io.Serializable;
+
+/**
+ * 通用业务消息接口.
+ * 各业务系统应定义一个枚举类实现该接口。
+ * 约定：
+ * 以"1"开头的为系统级别的消息。
+ * 各业务系统实现此接口时，不能使用以"1"开头的respCode.
+ *
+ * @author SUNYAFEI
+ * @version : 1.0
+ */
+public interface Message extends Serializable {
+
+    String getRespCode();
+
+    String getRespMsg();
+
+}
