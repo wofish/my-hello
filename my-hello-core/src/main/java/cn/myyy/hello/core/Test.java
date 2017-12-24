1e55fc4d31047c602e6e956bd46e3ea0922a587b
syfei49@163.com <syfei49@163.com>
2017-12-24 21:45:00 +0800

    增加静态配置

---
+++ my-hello-core/src/main/java/cn/myyy/hello/core/Test.java
@@ -0,0 +1,9 @@
+package cn.myyy.hello.core;
+
+/**
+ * @project: my-hello
+ * @date: 2017/12/24
+ * @author: SUNYAFEI
+ */
+public class Test {
+}
---
+++ my-hello-core/src/main/java/cn/myyy/hello/core/business/Test.java
@@ -0,0 +1,9 @@
+package cn.myyy.hello.core.business;
+
+/**
+ * @project: my-hello
+ * @date: 2017/12/24
+ * @author: SUNYAFEI
+ */
+public class Test {
+}
